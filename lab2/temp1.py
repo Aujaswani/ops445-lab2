0debe126b3aad3108ace9af047a741994d3d1105
Aujaswani Rajput <aujaswani@myseneca.ca>
2025-02-21 20:23:34 -0500

    completed lab

---
+++ lab2/temp1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+
+
+password = ''
+while password != '111':
+    password = input("Type in a password: ")
+    
+
+print('Congratulations you guessed the correct password!')
